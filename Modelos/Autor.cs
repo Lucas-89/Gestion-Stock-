59c539f73d7843b355f4645b0a40ddd07c3807f9
Lucas Mansilla <mansillalucas1212@gmail.com>
2023-02-01 20:21:01 -0300

    Propiedad ID

--- Modelos/Autor.cs
+++ Modelos/Autor.cs
@@ -7,6 +7,7 @@ namespace GestionStock.Modelos
 {
     public class Autor
     {
+        public int Id{get;set;}
         public string Nombre{get;set;}
         public string Apellido{get;set;}
         public string Nacionalidad{get;set;}
--- Modelos/Libro.cs
+++ Modelos/Libro.cs
@@ -7,6 +7,7 @@ namespace GestionStock.Modelos
 {
     public class Libro
     {
+        public int Id{get;set;}
         public string Nombre{get;set;}
         public string Genero{get;set;}
         public int CantHojas{get;set;}
